4b6e3642d940cb96f404a8a8cf047466a5babb77
Omkar6627 <118187851+Omkar6627@users.noreply.github.com>
2023-07-21 20:40:58 +0530

    Update Stock.java

--- Stock-App/src/main/java/com/geekster/StockApp/model/Stock.java
+++ Stock-App/src/main/java/com/geekster/StockApp/model/Stock.java
@@ -1,7 +1,7 @@
 package com.geekster.StockApp.model;
 
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
